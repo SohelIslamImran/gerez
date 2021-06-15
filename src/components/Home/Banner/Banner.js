9a4401bd3b80c6d7f7e7cf78b561d664e7e8982b
SohelIslamImran <sohelislamimran@gmail.com>
2021-06-15 18:12:22 +0600

    Fixed some problems

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -7,7 +7,7 @@ const Banner = () => {
     return (
         <Container fluid>
             <Row className="align-items-center justify-content-center banner">
-                <Col md={4} className="p-md-5 order-2 order-md-1">
+                <Col lg={4} className="p-lg-5 order-2 order-lg-1">
                     <Fade left duration={2000} distance="40px">
                         <h1>We Make Car <br /> Repair Hassle Free</h1>
                         <p className="text-muted my-4 pr-md-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the</p>
@@ -18,7 +18,7 @@ const Banner = () => {
                         </Button>
                     </Fade>
                 </Col>
-                <Col md={6} className="order-1 order-md-2">
+                <Col lg={6} className="order-1 order-lg-2">
                     <Fade right duration={2000} distance="40px">
                         <Image src={bannerImg} fluid />
                     </Fade>
